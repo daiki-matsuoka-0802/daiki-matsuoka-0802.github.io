72516108ffa1378b28698a9bca2b52d66d48f2d6
daiki-matsuoka-0802 <92074317+daiki-matsuoka-0802@users.noreply.github.com>
2025-11-08 09:14:18 +0000

    Deploying to gh-pages from @ daiki-matsuoka-0802/daiki-matsuoka-0802.github.io@fb9b7ebb8e737ff69941cc198236ef3379a7fff4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,39 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/presentations/";
           },
-        },{
+        },<<<<<<< HEAD=======>>>>>>> upstream/main{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2.html";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%64%61%69%6B%69.%6D%61%74%73%75%6F%6B%61@%69%73.%73.%75-%74%6F%6B%79%6F.%61%63.%6A%70", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0008-4378-6580", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=tGtu4IsAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
